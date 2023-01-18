8dd46af205ef3e1e1a1bedbc9e60e78b0475fd72
Kyu Lee <kwooler@gmail.com>
2023-01-18 08:26:03 -0800

    L43

--- main_test.py
+++ main_test.py
@@ -7,7 +7,7 @@ import re
 def test_main_1():
     captureOut = io.StringIO()
     sys.stdout = captureOut
-    datastr = '10'
+    datastr = '5\n2\n3\n1\n5'
     sys.stdin = io.StringIO(datastr)
 
     main.main()
@@ -18,7 +18,7 @@ def test_main_1():
 
     # regex_string = r'[\w,\W]*' + str(minval) + r'[\w,\W]*'
     # res = re.search(regex_string, lines[0])
-    regex_string = r'[\w,\W]*1[\w,\W]*2[\w,\W]*4[\w,\W]*8[\w,\W]*16[\w,\W]*32[\w,\W]*64[\w,\W]*128[\w,\W]*256[\w,\W]*512[\w,\W]*1024[\w,\W]*'
+    regex_string = r'[\w,\W]*16[\w,\W]*'
     res = re.search(regex_string, lines[0])
     assert res != None
     print(res.group())
@@ -27,7 +27,7 @@ def test_main_1():
 def test_main_2():
     captureOut = io.StringIO()
     sys.stdout = captureOut
-    datastr = '11'
+    datastr = '1\n2\n3\n4\n5'
     sys.stdin = io.StringIO(datastr)
 
     main.main()
@@ -38,27 +38,7 @@ def test_main_2():
 
     # regex_string = r'[\w,\W]*' + str(minval) + r'[\w,\W]*'
     # res = re.search(regex_string, lines[0])
-    regex_string = r'[\w,\W]*Program[\w,\W]*[s,S]top[\w,\W]*'
-    res = re.search(regex_string, lines[0])
-    assert res != None
-    print(res.group())
-
-
-def test_main_3():
-    captureOut = io.StringIO()
-    sys.stdout = captureOut
-    datastr = '4'
-    sys.stdin = io.StringIO(datastr)
-
-    main.main()
-    sys.stdout = sys.__stdout__
-    print('Captured ', captureOut.getvalue())
-    lines = captureOut.getvalue().split('\n')
-    print(lines)
-
-    # regex_string = r'[\w,\W]*' + str(minval) + r'[\w,\W]*'
-    # res = re.search(regex_string, lines[0])
-    regex_string = r'[\w,\W]*1[\w,\W]*2[\w,\W]*4[\w,\W]*8[\w,\W]*16[\w,\W]*'
+    regex_string = r'[\w,\W]*15[\w,\W]*'
     res = re.search(regex_string, lines[0])
     assert res != None
     print(res.group())
