ece7f94f422347850c29d2dbb47b8e591ef04ee1
Kyu Lee <kwooler@gmail.com>
2023-01-18 08:38:33 -0800

    L43-List

--- main.py
+++ main.py
@@ -2,6 +2,15 @@ def main():
     ##################################################
     # Comlete your code here
     ##################################################
+
+    sum = 0
+    numbers = [0] * 5
+    for i in range(len(numbers)):
+        numbers[i] = int(input())
+        sum += numbers[i]
+        # print (numbers[i], end=' ')
+    print(f'Total is {sum}')
+
     pass
 
 
--- main_test.py
+++ main_test.py
@@ -42,3 +42,23 @@ def test_main_2():
     res = re.search(regex_string, lines[0])
     assert res != None
     print(res.group())
+
+
+def test_main_3():
+    captureOut = io.StringIO()
+    sys.stdout = captureOut
+    datastr = '10\n20\n30\n40\n50'
+    sys.stdin = io.StringIO(datastr)
+
+    main.main()
+    sys.stdout = sys.__stdout__
+    print('Captured ', captureOut.getvalue())
+    lines = captureOut.getvalue().split('\n')
+    print(lines)
+
+    # regex_string = r'[\w,\W]*' + str(minval) + r'[\w,\W]*'
+    # res = re.search(regex_string, lines[0])
+    regex_string = r'[\w,\W]*150[\w,\W]*'
+    res = re.search(regex_string, lines[0])
+    assert res != None
+    print(res.group())
