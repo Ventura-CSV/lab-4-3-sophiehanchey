b6707b1e4b6797861e446d466693ad1488003009
Kyu Lee <kwooler@gmail.com>
2023-01-18 08:38:46 -0800

    L43-List

--- main.py
+++ main.py
@@ -2,15 +2,6 @@ def main():
     ##################################################
     # Comlete your code here
     ##################################################
-
-    sum = 0
-    numbers = [0] * 5
-    for i in range(len(numbers)):
-        numbers[i] = int(input())
-        sum += numbers[i]
-        # print (numbers[i], end=' ')
-    print(f'Total is {sum}')
-
     pass
 
 
