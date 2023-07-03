69ec4210d458a6c5d482556512885737a9806894
Kyuwoong Lee <kwooler@gmail.com>
2023-07-03 15:57:53 -0700

    Update main_test.py

--- main_test.py
+++ main_test.py
@@ -10,7 +10,7 @@ def test_main_1():
     datastr = '5\n2\n3\n1\n5'
     sys.stdin = io.StringIO(datastr)
 
-    main.main()
+    retval = main.main()
     sys.stdout = sys.__stdout__
     print('Captured ', captureOut.getvalue())
     lines = captureOut.getvalue().split('\n')
@@ -18,10 +18,11 @@ def test_main_1():
 
     # regex_string = r'[\w,\W]*' + str(minval) + r'[\w,\W]*'
     # res = re.search(regex_string, lines[0])
-    regex_string = r'[\w,\W]*16[\w,\W]*'
-    res = re.search(regex_string, lines[0])
-    assert res != None
-    print(res.group())
+    # regex_string = r'[\w,\W]*16[\w,\W]*'
+    # res = re.search(regex_string, lines[0])
+    # assert res != None
+    # print(res.group())
+    assert retval == 16
 
 
 def test_main_2():
@@ -30,7 +31,7 @@ def test_main_2():
     datastr = '1\n2\n3\n4\n5'
     sys.stdin = io.StringIO(datastr)
 
-    main.main()
+    retval = main.main()
     sys.stdout = sys.__stdout__
     print('Captured ', captureOut.getvalue())
     lines = captureOut.getvalue().split('\n')
@@ -38,10 +39,11 @@ def test_main_2():
 
     # regex_string = r'[\w,\W]*' + str(minval) + r'[\w,\W]*'
     # res = re.search(regex_string, lines[0])
-    regex_string = r'[\w,\W]*15[\w,\W]*'
-    res = re.search(regex_string, lines[0])
-    assert res != None
-    print(res.group())
+    # regex_string = r'[\w,\W]*15[\w,\W]*'
+    # res = re.search(regex_string, lines[0])
+    # assert res != None
+    # print(res.group())
+    assert retval == 15
 
 
 def test_main_3():
@@ -50,7 +52,7 @@ def test_main_3():
     datastr = '10\n20\n30\n40\n50'
     sys.stdin = io.StringIO(datastr)
 
-    main.main()
+    retval = main.main()
     sys.stdout = sys.__stdout__
     print('Captured ', captureOut.getvalue())
     lines = captureOut.getvalue().split('\n')
@@ -58,7 +60,8 @@ def test_main_3():
 
     # regex_string = r'[\w,\W]*' + str(minval) + r'[\w,\W]*'
     # res = re.search(regex_string, lines[0])
-    regex_string = r'[\w,\W]*150[\w,\W]*'
-    res = re.search(regex_string, lines[0])
-    assert res != None
-    print(res.group())
+    # regex_string = r'[\w,\W]*150[\w,\W]*'
+    # res = re.search(regex_string, lines[0])
+    # assert res != None
+    # print(res.group())
+    assert retval == 150
