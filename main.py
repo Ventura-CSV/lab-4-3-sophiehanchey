c07fa2e656f633e6ccbc09e88f1a4afdcbd624ae
Sophie Hanchey <sophiehanchey@gmail.com>
2024-06-28 11:31:16 -0700

    initial code, passed pytest

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 def main():
-    """
-    ########################################
-    Code Your Program here
-    ########################################
-    """
+    
     total = 0
+    i = 0
+    
+    # while loop 
+    while i<5: 
+        new_num = int(input('Please enter a number: '))
+        total += new_num
+        
+        i += 1
+        
+    print(f'Your total is: {total}')
 
     ########################################
     # Do not delete the return statement
